15b9b03b1ad430f070bbbdef4deeb98070dc12d0
agarcia <mercuriete@yahoo.es>
2024-10-13 16:46:35 +0100

    ci: implement simple cicd (#6)
    Co-authored-by: mercuriete <mercuriete@gmail.com>

--- pytest_example/main.py
+++ pytest_example/main.py
@@ -2,6 +2,6 @@ from pytest_example.hello_world import HelloWorld
 from pytest_example.hello_world_impl import HelloWorldImpl
 
 
-hello:HelloWorld = HelloWorldImpl()
+hello: HelloWorld = HelloWorldImpl()
 
 print(hello.getString())
